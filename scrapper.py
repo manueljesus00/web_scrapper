543b2a8bcef6beb9e33303a22961e3d39bb280d2
Manuel Jesus Flores Montaño <mjfloresmontano@gmail.com>
2019-09-14 11:53:17 +0200

    Upload

--- scrapper.py
+++ scrapper.py
@@ -3,25 +3,21 @@ import requests
 import urllib
 url = "https://www.directoalpaladar.com/categoria/postres/record/20"
 webpage=str(urllib.request.urlopen(url).read())
-soup = BeautifulSoup(webpage)
 counter = 0
 r = requests.get(url)
 code_status = r.status_code
-if code_status == 200:
-    html = BeautifulSoup(webpage, 'html.parser')
-    entradas = html.find_all('article', {'class': 'recent-abstract abstract-title'})
-    entradas2 = html.find_all('article', {'class': 'recent-abstract abstract-title m-featured'})
 
-    for item in entradas2:
-        entradas.append(item)
+if code_status == 200:
+    html = BeautifulSoup(webpage, features='html.parser')
+    entradas = html.find_all('article')
     for item in entradas:
-        cont += 1
+        counter += 1
         titulo = item.find('h2', {'class':'abstract-title'}).getText()
         link = item.find('a').get('href')
 
-        print("Id........: %d" % cont)
-        print("Titulo....: " + titulo)
+        print("Id........: %d" % counter)
+        print("Titulo....: " + cor_titulo)
         print("Link......: " + link)
         print("______________________________________")
 else:
-    print ("Error "+code_status+" en la página")
+    print ("Error en la página")
