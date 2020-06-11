72aa286a0c4af9b618cf9395c7cf73023873a054
manueljesus00 <mjfloresmontano@gmail.com>
2020-06-11 21:00:23 +0200

    Update

--- scrapper.py
+++ scrapper.py
@@ -1,20 +1,30 @@
 from bs4 import BeautifulSoup
-import requests
+import urllib
 
-url = "https://www.directoalpaladar.com/categoria/postres/record/20"
-counter = 0
-r = requests.get(url)
-soup = BeautifulSoup(r.text,'lxml')
-articulos = soup.find_all('h2', {'class':'abstract-title'})
+url = 'http://localhost/' #Insert URL here
+html = urllib.request.urlopen(url)
+soup = BeautifulSoup(html)
+tags = soup('a')
+print('Enlaces de la página principal: \r\n')
+for tag in tags:
+    print(tag.contents[0], tag.get('href'))
 
-if len(articulos) > 0:
-    for articulo in articulos:
-        counter += 1
-        link = articulo.find('a').get('href')
-        titulo = articulo.get_text()
-        print("Id........: %d" % counter)
-        print("Titulo....: " + titulo)
-        print("Link......: " + link)
-        print("______________________________________")
-else:
-    print ("Error en la página")
+print('\r\n Enlaces de las páginas secundarias: \r\n')
+for tag in tags:
+    newurl = tag.get('href')
+    print('* Accediendo a los enlaces dentro de la página', newurl)
+    try:
+        if newurl[0:4] == 'http':
+            html2 = urllib.request.urlopen(newurl)
+        else:
+            html2 = urllib.request.urlopen(url+urlnew)
+        soup2 = BeautifulSoup(html2)
+        newtags = soup2('a')
+        if len(newtags) > 0:
+            print(len(newtags), 'enlaces:')
+            for newtag in newtags:
+                print(newtag.get('href'))
+        else:
+            print('No hay más enlaces')
+    except:
+        print('Algo ha fallado')
